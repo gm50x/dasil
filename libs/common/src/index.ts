5c8b63de71dfaa696bb99e710fd0501b9b04fcd2
Getulio <getuliomagela@outlook.com>
2020-09-29 16:50:43 -0300

    feat: include decorators and errors to lib index

--- libs/common/src/index.ts
+++ libs/common/src/index.ts
@@ -1,2 +1,7 @@
+export * from './decorators/in-headers.decorator';
+
+export * from './errors/request-error';
+export * from './errors/server-error';
+
 export * from './common.module';
 export * from './common.service';
