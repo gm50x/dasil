7fba8b3011a484247cc0f0e96bfe85e0badf0651
Getulio <getuliomagela@outlook.com>
2020-09-30 13:53:23 -0300

    feat: include validation of input

--- src/core/feature-toggle/use-cases/get-feature-toggle-by-name/io/get-feature-toggle-by-name.input.ts
+++ src/core/feature-toggle/use-cases/get-feature-toggle-by-name/io/get-feature-toggle-by-name.input.ts
@@ -1,7 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsAlpha, IsNotEmpty } from 'class-validator';
 
 export class GetFeatureToggleByNameInput {
   @ApiProperty()
+  @IsAlpha()
+  @IsNotEmpty()
   toggleName: string;
 
   constructor(toggleName: string) {
