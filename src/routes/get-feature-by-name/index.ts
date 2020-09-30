62658ae263d41f368fdfcc239418dbbf4fbeb563
Getulio <getuliomagela@outlook.com>
2020-09-30 11:19:56 -0300

    feat: include features tag in swagger

--- src/routes/get-feature-by-name/index.ts
+++ src/routes/get-feature-by-name/index.ts
@@ -1,6 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller('features')
+@ApiTags('Features')
 export class GetFeatureByNameRoute {
   @Get()
   async activate(): Promise<any> {
