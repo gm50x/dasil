de64b7823e53a7e3fbe0f5108ae75040befbc413
Getulio <getuliomagela@outlook.com>
2020-09-30 11:33:02 -0300

    chore: include mocks to common index

--- libs/common/src/index.ts
+++ libs/common/src/index.ts
@@ -2,3 +2,7 @@ export * from './decorators/in-headers.decorator';
 
 export * from './errors/request-error';
 export * from './errors/server-error';
+
+export * from './mocks/mock-environment.service';
+export * from './mocks/mock.repository';
+export * from './mocks/mock.use-case';
